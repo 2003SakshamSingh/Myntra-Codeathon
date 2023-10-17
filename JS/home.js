5a60726f58981f33414f5852c6612fba95e36c85
JAY-KUMAR-SAHU <jks17092002@gmail.com>
2023-10-17 23:19:52 +0530

    2 sliders added

--- JS/home.js
+++ JS/home.js
@@ -4,14 +4,25 @@ let header = document.getElementById("Home-headerEl");
 header.innerHTML = navbar();
 let footerEl = document.getElementById("home-footer");
 footerEl.innerHTML = footer();
-var counter = 1;
+/* BANNER SLIDESHOW IMAGE DISPLAY FUNCTION */
+var counter1 = 1;
 setInterval(function () {
-  document.getElementById("radio" + counter).checked = true;
-  counter++;
-  if (counter > 5) {
-    counter = 1;
+  document.getElementById("radio" + counter1).checked = true;
+  counter1++;
+  if (counter1 > 5) {
+    counter1 = 1;
   }
-}, 5000);
+}, 4000);
+
+/* FIRST PURCHASE SUCCESS STORIES SLIDESHOW IMAGE DISPLAY FUNCTION */
+var counter2 = 6;
+setInterval(function () {
+  document.getElementById("radio" + counter2).checked = true;
+  counter2++;
+  if (counter2 > 10) {
+    counter2 = 6;
+  }
+}, 3000);
 
 const cartCountInfo = document.getElementById("cart-count-info");
 let count = JSON.parse(localStorage.getItem("cart"));
