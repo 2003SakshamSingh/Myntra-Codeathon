90e5d7def271381ce2c33b83cda9c478c5c969cc
2003SakshamSingh <101103322+2003SakshamSingh@users.noreply.github.com>
2023-10-24 17:55:18 +0530

    Community posts feature added

--- JS/communityForum.js
+++ JS/communityForum.js
@@ -1,5 +1,5 @@
-for(let i=1; i<=5; i++){
-  document.getElementById("comm"+i).addEventListener("click", (e)=>{
-    document.getElementById("community-name").innerHTML=document.getElementById("comm"+i).textContent;
-  });
-}
+// for(let i=1; i<=5; i++){
+//   document.getElementById("comm"+i).addEventListener("click", (e)=>{
+//     document.getElementById("community-name").innerHTML=document.getElementById("comm"+i).textContent;
+//   });
+// }
