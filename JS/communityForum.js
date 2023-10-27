a1ed139c13bd05204f9de40aa2ca4f04d74ca61f
JAY-KUMAR-SAHU <jks17092002@gmail.com>
2023-10-27 23:30:09 +0530

    community forum slider completed

--- JS/communityForum.js
+++ JS/communityForum.js
@@ -6,10 +6,10 @@
 
 /* BANNER SLIDESHOW IMAGE DISPLAY FUNCTION */
 var slideCounter = 1;
-      setInterval(function () {
-        document.getElementById("radio" + slideCounter).checked = true;
-        slideCounter++;
-        if (slideCounter > 5) {
-          slideCounter = 1;
-        }
-      }, 4000);
+setInterval(function () {
+  document.getElementById("radio" + slideCounter).checked = true;
+  slideCounter++;
+  if (slideCounter > 5) {
+    slideCounter = 1;
+  }
+}, 4000);
