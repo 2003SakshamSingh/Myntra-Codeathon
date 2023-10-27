317671b3ede49294c1a7f5522119868834425cad
2003SakshamSingh <101103322+2003SakshamSingh@users.noreply.github.com>
2023-10-27 22:55:01 +0530

    Slider fixed

--- JS/communityForum.js
+++ JS/communityForum.js
@@ -3,3 +3,13 @@
 //     document.getElementById("community-name").innerHTML=document.getElementById("comm"+i).textContent;
 //   });
 // }
+
+/* BANNER SLIDESHOW IMAGE DISPLAY FUNCTION */
+var slideCounter = 1;
+      setInterval(function () {
+        document.getElementById("radio" + slideCounter).checked = true;
+        slideCounter++;
+        if (slideCounter > 5) {
+          slideCounter = 1;
+        }
+      }, 4000);
